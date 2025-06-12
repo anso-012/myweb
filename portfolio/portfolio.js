40140cabe9d9aab690a225f62c647ad3fe7a7518
anso-012 yeong <sohie.e03@gmail.com>
2025-06-12 10:45:04 +0000

    포트폴리오 웹페이지

--- portfolio/portfolio.js
+++
@@ -1,7 +0,0 @@
-function openModal(projectId) {
-  document.getElementById('modal').style.display = 'block';
-}
-
-function closeModal() {
-  document.getElementById('modal').style.display = 'none';
-}
